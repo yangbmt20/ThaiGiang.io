28b222f35c2a84690a2c22e50f8d907e3a73caaa
yangbmt20 <140824785+yangbmt20@users.noreply.github.com>
2023-07-28 20:41:16 +0700

    Add files via upload

---
+++ js/Slide-1.js
@@ -0,0 +1,17 @@
+$('#sp-men,#sp-women,#sp-kid').owlCarousel({
+    loop:true,
+	autoplay:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
